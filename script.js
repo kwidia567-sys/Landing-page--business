d7d299cb2b208494ef4fc4e63951317d7abcfdb6
kwidia567-sys <kwidia567@gmail.com>
2025-11-18 10:34:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efek scroll halus ke setiap bagian
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
